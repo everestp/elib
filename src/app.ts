89c312b40aa693a40298f79e5200b786ea10e573
Everest Paudel <paudeleverest09@gmail.com>
2025-08-10 11:14:09 +0545

    feat: remove Dockerfile and docker-compose, add database connection and configuration

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,13 @@ const app = express();
 app.use(express.json());
 
 // Routes
+
+
+
 // Http methods: GET, POST, PUT, PATCH, DELETE
+
+
+
 app.get("/", (req, res, next) => {
     res.json({ message: "Welcome to elib apis" });
 });
---
+++ src/config/db.ts
@@ -0,0 +1,24 @@
+import { config } from './config'
+import mongoose from 'mongoose'
+
+
+const connectDB = async ()=>{
+
+    try {
+        mongoose.connection.on('connected',()=>{
+            console.log("Connection to databases sucessfully")
+        })
+
+        mongoose.connection.on('error',(err)=>{
+            console.log("Error in connecting databases",err)
+        })
+        await mongoose.connect(config.databaseUrl as string)
+        
+        
+    } catch (error) {
+        console.error("Failed to connect to database",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
