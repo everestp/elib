838f20d8f21a4232718ad17b03c8bba1cc6b506c
Everest Paudel <paudeleverest09@gmail.com>
2025-08-14 17:37:37 +0545

    feat: enhance user creation and login logic with improved error handling and validation

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,99 +1,105 @@
-import { NextFunction  , Request , Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
 
 import { User } from "./userTypes";
 
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password } = req.body;
 
+    if (!name || !email || !password) {
+        const error = createHttpError(400, "All field are required");
+        return next(error);
+    }
 
-const createUser =  async (req:Request ,res:Response , next:NextFunction)=>{
- const { name ,email , password} = req.body;
+    //Process to create the user
+    //Database call
+    try {
+        const user = await userModel.findOne({ email });
+        if (user) {
+            const error = createHttpError(
+                400,
+                "User already exist with this email"
+            );
+            return next(error);
+        }
+    } catch (error) {
+        return next(createHttpError(500, "Error while geting user"));
+    }
+    // if not user add user to the dataBase
+    const hashedPassword = await bcrypt.hash(password, 10);
+    let newUser: User;
+    try {
+        newUser = await userModel.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+    } catch (error) {
+        return next(createHttpError(500, "Error while creating user"));
+    }
 
- if(!name || !email || !password){
-    const error = createHttpError(400,"All field are required" )
-    return next(error)
-    
- }
- 
- //Process to create the user
- //Database call
- try {
-    const user = await userModel.findOne({email})
- if(user){
-   const error = createHttpError(400 ,"User already exist with this email")
-   return next(error)
- }
+    //Token generation JWT
 
-} catch (error) {
-  return next(createHttpError(500 ,"Error while geting user"))
-}
- // if not user add user to the dataBase
- const hashedPassword =  await bcrypt.hash(password , 10)
-let newUser :User
- try {
-   newUser =  await userModel.create({
-   name,
-   email,
-   hashedPassword
- })
- } catch (error) {
-   return next(createHttpError(500 ,"Error while creating user"))
- }
+    try {
+        const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+            expiresIn: "7d",
+            algorithm: "HS256",
+        });
+        return res.json({ accessToken: token });
+    } catch (error) {
+        return next(createHttpError(500, "Errror while siging jwt token"));
+    }
+};
 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
 
- //Token generation JWT
- 
- try {
- const token = sign({sub :newUser._id} ,config.jwtSecret as string ,{expiresIn : "7d" ,algorithm:'HS256'})
-return  res.status(201).json({accessToken :token})
- } catch (error) {
-   return next(createHttpError(500 ,"Errror while siging jwt token"))
- }
- 
+        // 1. Validate incoming request body
+        if (!email || !password) {
+            return next(
+                createHttpError(400, "Email and password are required.")
+            );
+        }
 
+        // 2. Find the user in the database
+        const user: User | null = await userModel.findOne({ email });
 
+        // Authenticate the user.
+        // Use a generic error message for security to prevent user enumeration.
+        if (!user) {
+            return next(createHttpError(404, "Invalid email or password."));
+        }
 
+        // Compare the provided password with the stored hashed password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return next(createHttpError(404, "Invalid email or password."));
+        }
 
- 
- 
- 
+        // Generate a JSON Web Token (JWT)
 
-}
+        const token = sign({ sub: user._id }, config.jwtSecret as string, {
+            expiresIn: "7d",
+            algorithm: "HS256",
+        });
 
+        // Send a successful response with the JWT token
+        return res.status(200).json({
+            message: "Login successful.",
+            accessToken: token,
+        });
+    } catch (error) {
+        console.error("Login error:", error);
 
+        return next(
+            createHttpError(500, "An unexpected error occurred during login.")
+        );
+    }
+};
 
-
-const loginUser = async (req:Request ,res:Response ,next:NextFunction)=>{
-const {email ,password} = req.body;
- console.log(email,password)
-if(!email || !password){
- const error = createHttpError(400 ,"Please fill the required field")
- return next(error)
-}
- 
-try {
-   const user = await userModel.findOne({email})
-   if(user){
-       const isMatch = await bcrypt.compare(email, user.email);
-      return isMatch
-   }
-  else{
-      return next(createHttpError(404 ,"Please enter valid id and passeword"))
-  }
-   
-   
-   
-} catch (error) {
-   return  next(createHttpError(404 ,"Invalid login Crendintals "))
-}
-
-
-
-
-
-}
-
-export {createUser , loginUser}
+export { createUser, loginUser };
