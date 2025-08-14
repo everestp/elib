2618c5b102091e9ab17a23c63e28116acf5c7fa5
Everest Paudel <paudeleverest09@gmail.com>
2025-08-14 15:16:12 +0545

    feat: add JWT token generation and user creation logic

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -2,6 +2,9 @@ import { NextFunction  , Request , Response} from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from 'bcrypt'
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { access } from "fs";
 
 
 
@@ -23,13 +26,19 @@ const createUser =  async (req:Request ,res:Response , next:NextFunction)=>{
 
  // if not user add user to the dataBase
  const hashedPassword =  await bcrypt.hash(password , 10)
+ const newUser =  await userModel.create({
+   name,
+   email,
+   hashedPassword
+ })
  
- 
+ //Token generation JWT
+const token = sign({sub :newUser._id} ,config.jwtSecret as string ,{expiresIn : "7d" ,algorithm:'HS256'})
  
  
  
  //Response the data
- return  res.json({message :"We are processing your request please hold on"})
+ return  res.json({accessToken :token})
 }
 
 export {createUser}
