0706c7dc7466ffdac5794c39fcfcbcd4e0164a96
Everest Paudel <paudeleverest09@gmail.com>
2025-08-14 19:49:00 +0545

    setup Multer

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,13 +1,27 @@
 import express from 'express'
 import { createBook, deleteBook, updateBook } from './bookController'
+import multer from 'multer'
+import path from 'node:path'
 
 
 const bookRouter = express.Router()
 
+
+//  for parsing multi part file because express cannot do by default
+const upload = multer({
+
+dest :path.resolve(__dirname,'../../public/data/uploads'),
+limits : {fileSize :3e7}
+
+})
 // creates a routes
 //Routes to add  book
 
-bookRouter.post("/add",createBook)
+bookRouter.post("/add",upload.fields([
+    {name: 'coverImage' ,maxCount:1},
+    {name: 'file' ,maxCount:1},
+
+]),createBook)
 
 bookRouter.patch("/update",updateBook)
 
